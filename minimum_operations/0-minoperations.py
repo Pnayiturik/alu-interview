a2b30180b6ebbd1d0632500502bfefc4578ad885
Pnayiturik <p.nayiturik@alustudent.com>
2024-05-17 14:40:00 +0200

    minoperations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to calculate minimum operations to get n H characters."""
+
+
+def minOperations(n):
+    list_of_factors = []
+    while n > 1:
+        for j in range(2, n + 1):
+            if n % j == 0:
+                list_of_factors.append(j)
+                n = int(n / j)
+                break
+    return sum(list_of_factors)
